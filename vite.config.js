ba20205964ee0bb5e4c0260a17434549a09a6d01
vptquyenn <quyenb1910439@student.ctu.edu.vn>
2022-10-29 13:56:07 +0700

    Hien thi danh sach cac contact lay ve tu server

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,19 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 3001,
-  },
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    port: 3001,
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000/",
+        changeOrigin: true,
+      },
+    }
   }
-})
+});
